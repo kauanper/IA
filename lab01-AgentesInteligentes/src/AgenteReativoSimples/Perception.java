e9a90366b5306d078fbf914e84410f5326752dd0
kauan <kauancavalcante8p@gmail.com>
2025-11-27 19:33:13 -0300

    add: perception

---
+++ lab01-AgentesInteligentes/src/AgenteReativoSimples/Perception.java
@@ -0,0 +1,11 @@
+package AgenteReativoSimples;
+
+public class Perception {
+    public char location;
+    public boolean isDirty;
+
+    public Perception(char location, boolean isDirty) {
+        this.location = location;
+        this.isDirty = isDirty;
+    }
+}
