b44540416aa0811957c856b13df91a3718066484
kauan <kauancavalcante8p@gmail.com>
2025-11-28 08:25:42 -0300

    add: heristicas

--- Buscas/src/grafo/Estado.java
+++ Buscas/src/grafo/Estado.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 public class Estado {
     public String nome;
+    public int Heuristica;
     public List<Transicao> transicoes;
 
-    public Estado(String nome) {
+
+    public Estado(String nome, int Heuristica) {
         this.nome = nome;
+        this.Heuristica = Heuristica;
         this.transicoes = new ArrayList<>();
     }
 }
--- Buscas/src/grafo/Romenia.java
+++ Buscas/src/grafo/Romenia.java
@@ -8,26 +8,26 @@ public class Romenia {
 
     public void init() {
 
-        Estado arad = new Estado("Arad");
-        Estado zerind = new Estado("Zerind");
-        Estado oradea = new Estado("Oradea");
-        Estado sibiu = new Estado("Sibiu");
-        Estado timisoara = new Estado("Timisoara");
-        Estado lugoj = new Estado("Lugoj");
-        Estado mehadia = new Estado("Mehadia");
-        Estado drobeta = new Estado("Drobeta");
-        Estado craiova = new Estado("Craiova");
-        Estado rimnicuVilcea = new Estado("Rimnicu Vilcea");
-        Estado fagaras = new Estado("Fagaras");
-        Estado pitesti = new Estado("Pitesti");
-        Estado bucharest = new Estado("Bucharest");
-        Estado giurgiu = new Estado("Giurgiu");
-        Estado urziceni = new Estado("Urziceni");
-        Estado hirsova = new Estado("Hirsova");
-        Estado eforie = new Estado("Eforie");
-        Estado vaslui = new Estado("Vaslui");
-        Estado iasi = new Estado("Iasi");
-        Estado neamt = new Estado("Neamt");
+        Estado arad = new Estado("Arad", 366);
+        Estado zerind = new Estado("Zerind", 374);
+        Estado oradea = new Estado("Oradea", 380);
+        Estado sibiu = new Estado("Sibiu", 253);
+        Estado timisoara = new Estado("Timisoara", 329);
+        Estado lugoj = new Estado("Lugoj", 244);
+        Estado mehadia = new Estado("Mehadia", 241);
+        Estado drobeta = new Estado("Drobeta", 242);
+        Estado craiova = new Estado("Craiova", 160);
+        Estado rimnicuVilcea = new Estado("Rimnicu Vilcea", 193);
+        Estado fagaras = new Estado("Fagaras", 176);
+        Estado pitesti = new Estado("Pitesti", 100);
+        Estado bucharest = new Estado("Bucharest", 0);
+        Estado giurgiu = new Estado("Giurgiu", 77);
+        Estado urziceni = new Estado("Urziceni", 80);
+        Estado hirsova = new Estado("Hirsova", 151);
+        Estado eforie = new Estado("Eforie", 161);
+        Estado vaslui = new Estado("Vaslui", 199);
+        Estado iasi = new Estado("Iasi", 226);
+        Estado neamt = new Estado("Neamt", 234);
 
         estados.add(arad);
         estados.add(zerind);
@@ -57,73 +57,92 @@ public class Romenia {
         arad.transicoes.add(new Transicao(zerind, 75));
         arad.transicoes.add(new Transicao(sibiu, 140));
         arad.transicoes.add(new Transicao(timisoara, 118));
+
         // Zerind
         zerind.transicoes.add(new Transicao(arad, 75));
         zerind.transicoes.add(new Transicao(oradea, 71));
+
         // Oradea
         oradea.transicoes.add(new Transicao(zerind, 71));
         oradea.transicoes.add(new Transicao(sibiu, 151));
+
         // Sibiu
         sibiu.transicoes.add(new Transicao(oradea, 151));
         sibiu.transicoes.add(new Transicao(arad, 140));
         sibiu.transicoes.add(new Transicao(fagaras, 99));
         sibiu.transicoes.add(new Transicao(rimnicuVilcea, 80));
+
         // Timisoara
         timisoara.transicoes.add(new Transicao(arad, 118));
         timisoara.transicoes.add(new Transicao(lugoj, 111));
+
         // Lugoj
         lugoj.transicoes.add(new Transicao(timisoara, 111));
         lugoj.transicoes.add(new Transicao(mehadia, 70));
+
         // Mehadia
         mehadia.transicoes.add(new Transicao(lugoj, 70));
         mehadia.transicoes.add(new Transicao(drobeta, 75));
+
         // Drobeta
         drobeta.transicoes.add(new Transicao(mehadia, 75));
         drobeta.transicoes.add(new Transicao(craiova, 120));
+
         // Craiova
         craiova.transicoes.add(new Transicao(drobeta, 120));
         craiova.transicoes.add(new Transicao(rimnicuVilcea, 146));
         craiova.transicoes.add(new Transicao(pitesti, 138));
+
         // Rimnicu Vilcea
         rimnicuVilcea.transicoes.add(new Transicao(sibiu, 80));
         rimnicuVilcea.transicoes.add(new Transicao(craiova, 146));
         rimnicuVilcea.transicoes.add(new Transicao(pitesti, 97));
+
         // Fagaras
         fagaras.transicoes.add(new Transicao(sibiu, 99));
         fagaras.transicoes.add(new Transicao(bucharest, 211));
+
         // Pitesti
         pitesti.transicoes.add(new Transicao(rimnicuVilcea, 97));
         pitesti.transicoes.add(new Transicao(craiova, 138));
         pitesti.transicoes.add(new Transicao(bucharest, 101));
+
         // Bucharest
         bucharest.transicoes.add(new Transicao(fagaras, 211));
         bucharest.transicoes.add(new Transicao(pitesti, 101));
         bucharest.transicoes.add(new Transicao(giurgiu, 90));
         bucharest.transicoes.add(new Transicao(urziceni, 85));
+
         // Giurgiu
         giurgiu.transicoes.add(new Transicao(bucharest, 90));
+
         // Urziceni
         urziceni.transicoes.add(new Transicao(bucharest, 85));
         urziceni.transicoes.add(new Transicao(hirsova, 98));
         urziceni.transicoes.add(new Transicao(vaslui, 142));
+
         // Hirsova
         hirsova.transicoes.add(new Transicao(urziceni, 98));
         hirsova.transicoes.add(new Transicao(eforie, 86));
+
         // Eforie
         eforie.transicoes.add(new Transicao(hirsova, 86));
+
         // Vaslui
         vaslui.transicoes.add(new Transicao(urziceni, 142));
         vaslui.transicoes.add(new Transicao(iasi, 92));
+
         // Iasi
         iasi.transicoes.add(new Transicao(vaslui, 92));
         iasi.transicoes.add(new Transicao(neamt, 87));
+
         // Neamt
         neamt.transicoes.add(new Transicao(iasi, 87));
     }
 
     public Estado getEstado(String termo) {
         for (Estado estado : estados) {
-            if(estado.nome.equals(termo)) {
+            if (estado.nome.equals(termo)) {
                 return estado;
             }
         }
