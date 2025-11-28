f51fcd349f51941b3e845ce7e31351cea139e30e
kauan <kauancavalcante8p@gmail.com>
2025-11-28 01:53:12 -0300

    busca gulosa

--- lab04-Heuristicas/src/BuscaGulosa.java
+++ lab04-Heuristicas/src/BuscaGulosa.java
@@ -1,14 +1,54 @@
 import grafos.Estado;
+import grafos.Transicao;
 
-import java.sql.ClientInfoStatus;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BuscaGulosa {
 
     public List<No> execute (Estado inicial, Estado destino){
-        List<No> caimnho = new ArrayList<No>();
+        List<No> caminho = new ArrayList<No>();
+        No raiz = new No(inicial, null, null, 0);
+        if(raiz.cidadeAtual.equals(destino)){
+            caminho.add(raiz);
+            return caminho;
+        }
 
-        return caimnho;
+        PriorityQueue<No> borda = new PriorityQueue<>(
+                Comparator.comparingInt(n -> n.cidadeAtual.Heuristica)
+        );
+        borda.add(raiz);
+        List<Estado> explorados = new ArrayList<>();
+
+        while(!borda.isEmpty()){
+            No u = borda.poll();
+            explorados.add(u.cidadeAtual);
+
+            if(u.cidadeAtual.equals(destino)){
+                while(u.pai != null){
+                    caminho.add(u);
+                    u = u.pai;
+                }
+                caminho.add(raiz);
+                return caminho;
+            }
+
+            for(Transicao acao : u.cidadeAtual.transicoes){
+                No filho = new No(acao.estado, u, acao, acao.custo + u.custoAcumulado);
+
+                if(explorados.contains(filho.cidadeAtual)){
+                    continue;
+                }
+
+                borda.add(filho);
+
+            }
+
+        }
+
+
+
+
+
+        return caminho;
     }
 }
